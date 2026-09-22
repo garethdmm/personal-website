24de5fba20544542a5eccef3f78c928d3e64b4a1
Gareth MacLeod <gareth.macleod@gmail.com>
2026-09-22 16:35:23 -0400

    Update intro text

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             I've been building startups since 2010—my own and others—to varying degrees of success. Presently I'm head of engineering at <a href='https://invertbio.com/' target='_blank'>Invert</a>, where we're building AI that can solve bioprocess development.
           </p>
           <p>
-            I look for problems where I can make a unique contribution; where something might not get done unless I do it. This impulse has shaped my career.
+            I try to spend my time on the most important thing that might not get done unless I do it.
           </p>
           <p>
             <a href="https://github.com/garethdmm" target="_blank">github</a>
